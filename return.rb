5dbf6860aa35cc1ce4aa8d15ef5bb8795e83d408
shumaikunkun <s1711520@gmail.com>
2019-08-27 16:06:11 +0900

    test

--- return.rb
+++ return.rb
@@ -1,6 +1,15 @@
 require 'json'
 
 def lambda_handler(event:, context:)
-    # TODO implement
-    { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
+    res = {
+        event: {
+            class: event.class,
+            inspect: event.inspect,
+        },
+        context: {
+            class: context.class,
+            inspect: context.inspect,
+        },
+    }
+    { statusCode: 200, body: res.to_json }
 end
