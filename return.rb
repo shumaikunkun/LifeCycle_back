6fa57da8e30d705d7e4a61cbdaf3d6f23bd8edf3
shumaikunkun <s1711520@gmail.com>
2019-08-27 16:39:07 +0900

    おうむ返し

--- return.rb
+++ return.rb
@@ -1,15 +1,7 @@
 require 'json'
 
 def lambda_handler(event:, context:)
-    res = {
-        event: {
-            class: event.class,
-            inspect: event.inspect,
-        },
-        context: {
-            class: context.class,
-            inspect: context.inspect,
-        },
-    }
-    { statusCode: 200, body: res.to_json }
+    res=event["message"]
+
+    { statusCode: 200, body: res }
 end
