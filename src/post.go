6563906b36df398bb4d23152f3b185531c17cf11
shumaikunkun <s1711520@gmail.com>
2019-08-29 11:03:14 +0900

    データの追加および新規作成の部分をリファクタリング2

--- src/post.go
+++ src/post.go
@@ -20,30 +20,18 @@ def lambda_handler(event:, context:)
     content=event["content"]
 
     #データベースを特定
-    dynamoDB = Aws::DynamoDB::Resource.new(region: 'ap-northeast-1')
-    table = dynamoDB.table('Data')
+    table = Aws::DynamoDB::Resource.new(region: 'ap-northeast-1').table('Data')
 
     #追加する案件
     data={ posted: Time.new.strftime("%m/%d-%H:%M"), st: st, en: en, tag: tag, content: content }
+    #一致するユーザのデータベース上のデータ
+    db=table.get_item({key: { 'name' => name } })["item"]
 
     #ユーザが登録がまだされていなかったら新規登録、されていたら案件を追加
-    if table.get_item({key: { 'name' => name } })["item"].nil?
-      plan_arr=[data]
-    else
-      plan_arr=table.get_item({key: { 'name' => name } })["item"]["plan"].push(data)
-    end
+    plan_arr= db.nil? ? [data] : db["plan"].push(data)
 
-    json={
-      item: {
-        name: name,
-        plan: plan_arr
-      }
-    }
     #データを入力
-    table.put_item(json)
+    table.put_item(json={ item: { name: name, plan: plan_arr } })
 
-    #データを取得
-    resp = table.get_item({ key: { 'name' => name } })
-
-    { statusCode: 200, body: resp }
+    { statusCode: 200, body: json }
 end
