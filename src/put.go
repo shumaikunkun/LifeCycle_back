08900cba56e6137347ce56468e4269a516413a9d
shumaikunkun <s1711520@gmail.com>
2019-08-29 16:48:10 +0900

    各タスクの時間と得点を返すように機能追加

--- src/put.go
+++ src/put.go
@@ -12,13 +12,25 @@ def lambda_handler(event:, context:)
     dynamoDB = Aws::DynamoDB::Resource.new(region: 'ap-northeast-1')
     table = dynamoDB.table('Data')
 
-
     #データを取得
     resp = table.get_item({ key: { 'name' => name }})
+    #タグごとの総時間をハッシュに格納
+    resp["item"]["plan"].each{|a|hash[a["tag"]]+=a["duar"].to_i}
+    #タグごとの時間のパーセンテージを格納
+    hash_rate=Hash.new
+    hash.each{|k,v|hash_rate[k]=v*100.0/hash.values.inject(:+)}
+    #得点を計算
+    point=0
+    hash_rate.each do |k,v|  #開発と勉強はそのまま得点化
+        if k=="1"||k=="2"
+            point+=v
+        elsif k=="0"||k=="4"||k=="7"  #睡眠と運動と食事は半分だけ加算
+            point+=v/2
+        end
+    end
 
-    arr=resp["item"]["plan"]
+    hash["point"]=point
 
-    arr.each{|a|hash[a["tag"]]+= a["duar"].to_i}
 
     return { statusCode: 200, body: hash.to_json }
 end
